878cb7318de2bde46842a1133099d2e23cb24e4a
baishun <whidy@whidy.net>
2023-12-13 17:57:56 +0800

    优化程序

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 // @author       Whidy
 // @match        apaas5.testysdpaas.local/apaas/manage/ui/
 // @match        cloud.ysdpaas.local/awecloud/login/
+// @match        bgapi.wodcloud.com/web/
 // @match        *://localhost:5173/apaas/manage/ui/
 // @match        https://apaas5.ysdpaas.local/apaas/manage/ui/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=testysdpaas.local
@@ -14,43 +15,59 @@
 
 (function () {
   "use strict";
-  console.log("start");
+  const DEBUG = false;
+  const BGAPI_PASSWORD = "bg123456";
+  const TIMEOUT = 3000;
+  const domain = location.host;
+  DEBUG && console.log("start: ", domain);
   let count = 0;
   window.timer = setInterval(() => {
-    console.log(count++);
-    let img =
-      document.querySelector(".yzm_img img") ||
-      document.querySelector(".yzm img");
+    DEBUG && console.log(count++);
+    let img = null;
+    switch (domain) {
+      case "bgapi.wodcloud.com":
+        img = document.querySelector(".v_code_img");
+        break;
+      case "cloud.ysdpaas.local":
+        img = document.querySelector(".yzm .yzm_img");
+        break;
+      default:
+        // apaas
+        img = document.querySelector(".yzm_img img");
+        break;
+    }
+    DEBUG && console.log(img);
     if (img) {
       img.addEventListener("load", function () {
-        // async function getCaptchaNumber() {
-        //   const response = await fetch("http://localhost:3000");
-        //   const data = await response.json();
-        //   console.log(data);
-        // }
-        // getCaptchaNumber();
-
-        // var script = document.createElement("script");
-        // script.src = `http://localhost:3000/`;
-        // document.body.appendChild(script);
-
-        // script.onload = function () {
-        //   // 在这里可以认为脚本加载并执行完成
-        //   console.log("Script loaded and executed");
-
-        //   // 检查全局变量是否设置
-        //   if (window.number) {
-        //     console.log("Number:", window.number);
-        //   }
-        // };
-
         var iframe = document.createElement("iframe");
         iframe.src = "http://localhost:3000";
         document.body.appendChild(iframe);
+        iframe.style.display = "none";
 
         iframe.onload = function () {
+          let imgBase64 = "";
+          if (img.src.startsWith("data:image")) {
+            imgBase64 = img.src;
+          } else if (img.src.startsWith("http")) {
+            const canvas = document.createElement("canvas");
+
+            // 设置 Canvas 的宽高与图像相同
+            canvas.width = img.width;
+            canvas.height = img.height;
+
+            // 获取 Canvas 的 2D 上下文
+            const context = canvas.getContext("2d");
+
+            // 在 Canvas 上绘制图像
+            context.drawImage(img, 0, 0);
+
+            // 将 Canvas 内容转换为 Base64 数据
+            imgBase64 = canvas.toDataURL("image/png");
+          }
+
+          DEBUG && console.log(imgBase64);
           // 在这里可以认为iframe的内容已加载完成
-          iframe.contentWindow.postMessage(img.src, "*");
+          iframe.contentWindow.postMessage(imgBase64, "*");
         };
 
         window.addEventListener("message", function (event) {
@@ -60,15 +77,33 @@
           // 在控制台中打印消息内容
           console.log("Received message from B:", numberString);
           if (numberString.length < 4) {
-            console.log("错误数据，重新获取");
+           console.log("错误数据，重新获取");
             img.click();
             return;
           }
-          const input =
-            document.querySelector(".msg-code .el-input__inner") ||
-            document.querySelector(".yzm .el-input__inner");
-          input.value = numberString;
-          input.dispatchEvent(new Event("input"));
+          let vcodeInput = null;
+          let passwordInput = null;
+          switch (domain) {
+            case "bgapi.wodcloud.com":
+              // 输入文档查看密码
+              passwordInput = document.querySelector("input[type=password]");
+              passwordInput.value = BGAPI_PASSWORD;
+              passwordInput.dispatchEvent(new Event("input"));
+              img = document.querySelector(".v_code_img");
+              vcodeInput =
+                img.previousElementSibling.querySelector(".el-input__inner");
+              break;
+            case "cloud.ysdpaas.local":
+              vcodeInput = document.querySelector(".yzm .el-input__inner");
+              break;
+            default:
+              // apaas
+              vcodeInput = document.querySelector(".msg-code .el-input__inner");
+              break;
+          }
+
+          vcodeInput.value = numberString;
+          vcodeInput.dispatchEvent(new Event("input"));
         });
       });
       clearInterval(window.timer);
@@ -79,5 +114,5 @@
   setTimeout(() => {
     clearInterval(window.timer);
     window.timer = null;
-  }, 5000);
+  }, TIMEOUT);
 })();
